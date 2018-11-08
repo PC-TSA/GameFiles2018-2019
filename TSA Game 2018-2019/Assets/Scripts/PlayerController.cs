1dd70ce67db183b03e49200c66be75c2192f550a
AlanG <alan.t.goff@icloud.com>
2018-11-08 14:41:52 -0500

    I attempted movement and camera stuff
    It did not go well

---
+++ TSA Game 2018-2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+                               // Use this for initialization
+    void Start () {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+    
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+        transform.GetComponent<Rigidbody>().AddForce(0,-0.05f,0);
+        
+    }
+}
