79a9f3b0afcd411e43a8ba404a03cb9dfa8f9948
Gabriel Moncau (Desktop) <gabriel.moncau@hotmail.com>
2018-11-23 20:53:24 -0500

    Fixed/tweaked movement/view, first working kinect anim test

---
+++ TSA Game 2018-2019/Assets/Scripts/Player/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+    public GameObject bow; //The bow object attached to the left hand
+    public GameObject playerLeftHand;
+    public GameObject playerRightHand;
+    public bool bowActive;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+            toggleBow();
+	}
+
+    public void toggleBow()
+    {
+        bowActive = !bowActive;
+        bow.SetActive(!bow.activeSelf);
+    }
+}
---
+++ TSA Game 2018-2019/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject gameController;
+    public GameObject playerCamera;
+
+    //Movement & Rotation
+    public float mouseSensitivity = 100f;
+    public float xClamp; //How far up/down you can look
+    public float rotY; // rotation around the up/y axis
+    public float rotX; // rotation around the right/x axis
+
+    void Start () {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movementAndRotation();
+    }
+
+    public void movementAndRotation()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -xClamp, xClamp);
+
+        Quaternion headRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        Quaternion bodyRotation = Quaternion.Euler(transform.rotation.x, rotY, 0.0f);
+        playerCamera.transform.rotation = headRotation;
+        transform.rotation = bodyRotation;
+    }
+}
