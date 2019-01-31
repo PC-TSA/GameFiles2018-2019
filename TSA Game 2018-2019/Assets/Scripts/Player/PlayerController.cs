4f19bc6bfa51c7db12986a92a7f473149b2db520
Gabriel Moncau <gabriel.moncau@hotmail.com>
2019-01-30 22:34:49 -0500

    Birch forest, placed town buildings, vault door, dust particle systems
    From discord:
    "- Made town layout
    - Imported all buildings and fixed their materials
    - Made birch forest
    - Moved mountains outward
    - Imported vault door and made emissive material
    - Made dust particle system, both for the cube in the crater and wrote code for one to change it's particle spawning point to follow the player in preparation for the vault"

--- TSA Game 2018-2019/Assets/Scripts/Player/PlayerController.cs
+++ TSA Game 2018-2019/Assets/Scripts/Player/PlayerController.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public GameObject character;
+    public GameObject dustParticleSystem; //The particle system that spawns 'dust' particles
 
-    /*void Update()
+    private void Start()
     {
         
-    }*/
+    }
+
+    void Update()
+    {
+        var shape = dustParticleSystem.GetComponent<ParticleSystem>().shape;
+        shape.position = new Vector3(character.transform.localPosition.x, character.transform.localPosition.y + 2, character.transform.localPosition.z);
+    }
 }
