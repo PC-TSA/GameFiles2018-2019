23b7060837c65797b70223b5fa3ea976b495f7a5
AlanG <alan.t.goff@icloud.com>
2019-02-02 13:38:19 -0500

    Interaction System
    Okay for soem reason Unity is being shit weird, and I can't click on random things and its just terrible.
    
    So I made the pseudocode for the interaction system. Basically anything that can be interacted with when you get close enough and press E will have the Interactable script on it, then the code will doTheThing() when you press E. It should be pretty easy to fill the things in the inspector and actually writ ethe real code as I'm pretty sure the system should work.

---
+++ TSA Game 2018-2019/Assets/Scripts/Object Scripts/Interactable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//for some reason unity and visual studio are being weird, im getting constant bugs, so I'm just gonna write the pseudo code and you can fill it in
+public class Interactable : MonoBehaviour {
+   /* public string popUpText;//message to display when this object is being interacted with by the player
+
+    public GameObject teleportLocation;//set in inspector to teleport to if this item is a door
+
+    public List<string> dialogueList;//list of dialogue lines to be iterated through
+    public void interact()
+    {
+        if(gameObject.GetTag() == "item")
+        {
+            addToInventory();
+        }
+        else if (gameObject.GetTag() == "door")
+        {
+            //fadetoblack()
+            //teleport player to  teleportLocation
+        }
+        else if (gameObject.GetTag() == "NPC")
+        {
+            startDialogue();
+        }
+    }
+
+    void addToInventory()
+    {
+
+    }
+
+    IEnumerator dialogue()
+    {
+        int index = 0;//for iteration through the list of dialogue strings
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            /*if(index < dialogueList.length)
+            {
+                UI textObject.text = dialogueList.get(index)
+                UI dialogueBackgroundObject.setActive(true);
+                index++;
+            }
+            else
+            {
+                 UI textObject.text = null;
+                 UI dialogueBackgroundObject.setActive(false);
+                 yield return null;
+            }
+            
+        }
+
+    }
+        */
+}
---
+++ TSA Game 2018-2019/Assets/Scripts/Player/InteractScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//for some reason unity and visual studio are being weird, im getting constant bugs, so I'm just gonna write the pseudo code and you can fill it in
+public class InteractScript : MonoBehaviour {
+    /*
+    public GameObject playerCamera;
+    public GameObject previousItemLookedAt = null;
+    public GameObject itemLookedAt = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        checkForInteraction();
+        if(itemLookedAt != null)
+        {
+            //UI Textobject.text = itemLookedAt.GetComponent<Interactable>().popUpText;
+            //UI imageObject.setActive(true); //this would be like an image of an E key
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                interact();
+                itemLookedAt = null;
+            }
+        }
+    }
+
+    void checkForInteraction()
+    {
+        Ray ray = playerCamera.GetComponent<Camera>().ViewportPointToRay(Vector3.one / 2f);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(ray, out hitInfo,2f))//if somethign collides with a ray sent out 2 units
+        {
+            var objectHit = hitInfo.collider.GetComponent<Interactable>();//set a variable to the hit objects ineractable script
+            if(objectHit == null)//if the object didnt have the interactable script
+            {
+                itemLookedAt = null;
+            }
+            else if(objectHit != null && itemLookedAt != objectHit && itemLookedAt != previousItemLookedAt)//if the object has the interactable script, and is different from what was previously looked at
+            {
+                itemLookedAt = objectHit;
+            }
+        }
+    }
+
+    void interact()
+    {
+        itemLookedAt.GetComponent<Interactable>().interact();
+    }
+    */
+}
